3a3cc803115d8b880070e9aaa70f9ebc62597894
Damian Nolan <damiannolan@gmail.com>
2026-05-04 11:45:32 +0200

    feat: add decoder func and more examples

--- cmd/rlp/main.go
+++ cmd/rlp/main.go
@@ -12,6 +12,7 @@ package main
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 )
 
@@ -43,20 +44,102 @@ func Encode(data []byte) []byte {
 	return append(res, data...)
 }
 
+func Decode(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, errors.New("empty input")
+	}
+
+	b0 := data[0]
+
+	switch {
+	case b0 < 0x80:
+		return data[:1], nil
+	case b0 <= 0xb7:
+		length := int(b0 - 0x80)
+
+		if len(data) < 1+length {
+			return nil, errors.New("input too short")
+		}
+
+		out := data[1 : 1+length]
+		if length == 1 && out[0] < 0x80 {
+			return nil, errors.New("non-canonical single byte encoding")
+		}
+
+		return out, nil
+	case b0 <= 0xbf:
+		lenOfLen := int(b0 - 0xb7)
+
+		if len(data) < 1+lenOfLen {
+			return nil, errors.New("input too short for length")
+		}
+
+		lenBytes := data[1 : 1+lenOfLen]
+		if lenBytes[0] == 0 {
+			return nil, errors.New("non-canonical length encoding")
+		}
+
+		length := 0
+		for _, b := range lenBytes {
+			length = (length << 8) | int(b)
+		}
+
+		if length <= 55 {
+			return nil, errors.New("long form used for short string")
+		}
+
+		if len(data) < 1+lenOfLen+length {
+			return nil, errors.New("input too short for string")
+		}
+
+		return data[1+lenOfLen : 1+lenOfLen+length], nil
+	default:
+		return nil, errors.New("expected byte string, got list")
+	}
+}
+
 func main() {
-	enc := Encode([]byte("dog"))
 	// Example: "dog" -> 0x83 + "dog"
+	enc := Encode([]byte("dog"))
 	fmt.Printf("dog: %x\n", enc)
 
-	enc = Encode([]byte("a"))
+	dec, err := Decode(enc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("result: %s\n", dec)
+
 	// Example: "a" -> 0x61
+	enc = Encode([]byte("a"))
 	fmt.Printf("a:   %x\n", enc)
 
-	enc = Encode([]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+	dec, err = Decode(enc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("result: %s\n", dec)
+
 	// Example: "a" (repeated 55 times) ->
+	enc = Encode([]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
 	fmt.Printf("a:   %x\n", enc)
 
-	enc = Encode([]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+	dec, err = Decode(enc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("result: %s\n", dec)
+
 	// Example: "a" (repeated 60 times) -> 0xb83c616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161
+	enc = Encode([]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
 	fmt.Printf("a:   %x\n", enc)
+
+	dec, err = Decode(enc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("result: %s\n", dec)
 }
